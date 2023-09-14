d912dc59ac976b2be722c60880053581fa517ac7
k-pineda <karenp9310@gmail.com>
2023-09-14 12:44:14 -0400

    debugged login & signup

--- public/js/signup.js
+++ public/js/signup.js
@@ -1,53 +1,28 @@
-// Function to handle new blog post submission
-const newBlogPostHandler = async (event) => {
-    event.preventDefault();
-  
-    // Collect values from the form
-    const title = document.querySelector('#post-title').value.trim();
-    const content = document.querySelector('#post-content').value.trim();
-  
-    if (title && content) {
-      // Send a POST request to the create post API endpoint
-      const response = await fetch('/api/posts', {
-        method: 'POST',
-        body: JSON.stringify({ title, content }),
-        headers: { 'Content-Type': 'application/json' },
-      });
-  
-      if (response.ok) {
-        // If post creation is successful, reload the page to display the new post
-        document.location.reload();
-      } else {
-        // Display an alert with the error message if post creation fails
-        const errorMessage = await response.json();
-        alert(errorMessage.message);
-      }
+const signupFormHandler = async (event) => {
+  event.preventDefault();
+
+  // Collect values from the signup form
+  const name = document.querySelector('#name-signup').value.trim();
+  const email = document.querySelector('#email-signup').value.trim();
+  const password = document.querySelector('#password-signup').value.trim();
+
+  if (name && email && password) {
+    // Send a POST request to the signup API endpoint
+    const response = await fetch('/api/users', {
+      method: 'POST',
+      body: JSON.stringify({ name, email, password }),
+      headers: { 'Content-Type': 'application/json' },
+    });
+
+    if (response.ok) {
+      // If signup is successful, redirect the browser to the dashboard page
+      document.location.replace('/dashboard');
+    } else {
+      // Display an alert with the error message if signup fails
+      const errorMessage = await response.json();
+      alert(errorMessage.message);
     }
-  };
-  
-  // Function to handle blog post deletion
-  const deleteBlogPostHandler = async (event) => {
-    if (event.target.hasAttribute('data-id')) {
-      const postId = event.target.getAttribute('data-id');
-  
-      // Send a DELETE request to the delete post API endpoint
-      const response = await fetch(`/api/posts/${postId}`, {
-        method: 'DELETE',
-      });
-  
-      if (response.ok) {
-        // If post deletion is successful, reload the page to update the list of posts
-        document.location.reload();
-      } else {
-        // Display an alert with the error message if post deletion fails
-        const errorMessage = await response.json();
-        alert(errorMessage.message);
-      }
-    }
-  };
-  
-  // Add event listener for new blog post submission
-  document.querySelector('.new-blog-post-form').addEventListener('submit', newBlogPostHandler);
-  
-  // Add event listener for blog post deletion
-  document.querySelector('.blog-post-list').addEventListener('click', deleteBlogPostHandler);
+  }
+};
+
+document.querySelector('.signup-form').addEventListener('submit', signupFormHandler);
