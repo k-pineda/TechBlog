738cc90e4a9aa82fa905dc6bc187ed333fa34b33
k-pineda <karenp9310@gmail.com>
2023-09-14 12:29:42 -0400

    Update database configuration for Heroku

--- config/connection.js
+++ config/connection.js
@@ -1,21 +1,26 @@
-const Sequelize = require('sequelize');
+const { Sequelize } = require('sequelize');
 require('dotenv').config();
 
 let sequelize;
 
-if (process.env.JAWSDB_URL) {
-  sequelize = new Sequelize(process.env.JAWSDB_URL);
+if (process.env.DATABASE_URL) {
+  // Use Heroku Postgres URL
+  sequelize = new Sequelize(process.env.DATABASE_URL, {
+    dialect: 'postgres',
+    dialectOptions: {
+      ssl: {
+        rejectUnauthorized: false,
+      },
+    },
+  });
 } else {
-  sequelize = new Sequelize(
-    process.env.DB_NAME,
-    process.env.DB_USER,
-    process.env.DB_PASSWORD,
-    {
-      host: 'localhost',
-      dialect: 'mysql',
-      port: 3306
-    }
-  );
+  // Use your local database URL
+  sequelize = new Sequelize(process.env.DB_NAME, process.env.DB_USER, process.env.DB_PASSWORD, {
+    host: 'localhost',
+    dialect: 'mysql', // Change this to the appropriate database type (e.g., 'postgres')
+    port: 3306, // Change this to the appropriate port
+  });
 }
 
 module.exports = sequelize;
+
